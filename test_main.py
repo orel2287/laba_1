f788c6343b84edf6726dc799e81dd6b1b035f70e
unknown <jaymun2@yandex.ru>
2024-04-03 22:38:44 +0300

    first commit

---
+++ test_main.py
@@ -0,0 +1,24 @@
+import unittest
+from main import add_numbers
+
+
+class TestAddNumbers(unittest.TestCase):
+
+    def test_add_positive_numbers(self):
+        result = add_numbers(3, 5)
+        self.assertEqual(result, 8)
+
+    def test_add_negative_numbers(self):
+        result = add_numbers(-2, -4)
+        self.assertEqual(result, -6)
+
+    def test_add_mixed_numbers(self):
+        result = add_numbers(10, -3)
+        self.assertEqual(result, 7)
+
+    def test_add_irrational_numbers(self):
+        result = add_numbers(0.2, 0.3)
+        self.assertEqual(result, 0.5)
+
+if __name__ == '__main__':
+    unittest.main()
